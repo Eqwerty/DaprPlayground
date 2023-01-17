2b9e623307fb291326cd4c3397ddcc4df61a0b38
Eduardo Quintana <edquintest@gmail.com>
2023-01-17 18:08:50 +0100

    Update

--- src/MyActor.Service/Actors/MyActor.cs
+++ src/MyActor.Service/Actors/MyActor.cs
@@ -7,15 +7,17 @@ public class MyActor : Actor, IMyActor
 {
     public MyActor(ActorHost host) : base(host)
     { }
+    
+    private const string StateName = "userTopics";
 
     public async Task SetDataAsync(MyData data)
     {
-        await StateManager.SetStateAsync(Id.GetId(), data);
+        await StateManager.SetStateAsync(StateName, data);
     }
 
     public async Task<MyData?> GetDataAsync()
     {
-        var data = await StateManager.TryGetStateAsync<MyData>(Id.GetId());
+        var data = await StateManager.TryGetStateAsync<MyData>(StateName);
 
         return data.HasValue ? data.Value : null;
     }
