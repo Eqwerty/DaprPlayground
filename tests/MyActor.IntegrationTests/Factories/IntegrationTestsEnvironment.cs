9067282151bc309349f8d23487e4f5ae97cd47dd
Eduardo Quintana <edquintest@gmail.com>
2023-01-19 17:36:33 +0100

    Add Integration test environment with class fixture

---
+++ tests/MyActor.IntegrationTests/Factories/IntegrationTestsEnvironment.cs
@@ -0,0 +1,68 @@
+﻿using Dapr.Client;
+using Microsoft.Extensions.DependencyInjection;
+using MyActor.IntegrationTests.Dapr;
+using MyActor.IntegrationTests.Redis;
+using Xunit;
+
+namespace MyActor.IntegrationTests.Factories;
+
+public class IntegrationTestsEnvironment : IAsyncLifetime
+{
+    public static readonly DateTime UtcNow = DateTime.UtcNow;
+    public LoggerFactory LoggerFactory { get; private set; }
+    public ClientFactory ClientFactory { get; private set; }
+    public ServiceFactory ServiceFactory { get; private set; }
+
+    public async Task InitializeAsync()
+    {
+        await RedisContainer.StartAsync();
+
+        await DaprHelper.InitAsync(
+            Settings.Logger.AppId,
+            Settings.Logger.AppPort,
+            Settings.Logger.DaprHttpPort,
+            Settings.Logger.DaprGrpcPort,
+            Settings.Logger.ComponentsPath
+        );
+
+        await DaprHelper.InitAsync(
+            Settings.Service.AppId,
+            Settings.Service.AppPort,
+            Settings.Service.DaprHttpPort,
+            Settings.Service.DaprGrpcPort,
+            Settings.Service.ComponentsPath
+        );
+
+        await DaprHelper.InitAsync(
+            Settings.Client.AppId,
+            Settings.Client.AppPort,
+            Settings.Client.DaprHttpPort,
+            Settings.Client.DaprGrpcPort,
+            Settings.Client.ComponentsPath
+        );
+
+        ClientFactory = new();
+        ClientFactory.CreateClient();
+        var clientDaprClient = ClientFactory.Services.GetRequiredService<DaprClient>();
+        await clientDaprClient.WaitForSidecarAsync();
+
+        ServiceFactory = new();
+        ServiceFactory.CreateClient();
+        var serviceDaprClient = ServiceFactory.Services.GetRequiredService<DaprClient>();
+        await serviceDaprClient.WaitForSidecarAsync();
+
+        LoggerFactory = new(UtcNow);
+        LoggerFactory.CreateClient();
+        var loggerDaprClient = LoggerFactory.Services.GetRequiredService<DaprClient>();
+        await loggerDaprClient.WaitForSidecarAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await RedisContainer.DisposeAsync();
+
+        await DaprHelper.StopAsync(Settings.Logger.AppId);
+        await DaprHelper.StopAsync(Settings.Service.AppId);
+        await DaprHelper.StopAsync(Settings.Client.AppId);
+    }
+}
--- tests/MyActor.IntegrationTests/Factories/MyActorTests.cs
+++ tests/MyActor.IntegrationTests/Factories/MyActorTests.cs
@@ -1,12 +1,8 @@
 ﻿using System.Net;
 using System.Text;
-using Dapr.Client;
 using FluentAssertions;
 using FluentAssertions.Execution;
-using Microsoft.Extensions.DependencyInjection;
 using MyActor.Client.Requests;
-using MyActor.IntegrationTests.Dapr;
-using MyActor.IntegrationTests.Redis;
 using MyActor.Interfaces;
 using Newtonsoft.Json;
 using Xunit;
@@ -14,10 +10,8 @@ using Xunit.Abstractions;
 
 namespace MyActor.IntegrationTests.Factories;
 
-public class MyActorTests
+public class MyActorTests : IClassFixture<IntegrationTestsEnvironment>
 {
-    private static readonly DateTime UtcNow = DateTime.UtcNow;
-
     private readonly ITestOutputHelper _testOutputHelper;
 
     public MyActorTests(ITestOutputHelper testOutputHelper)
@@ -28,107 +22,54 @@ public class MyActorTests
     [Fact]
     public async Task MyActor_ShouldReturnOkResult_WhenDataIsSet()
     {
-        try
+        //Arrange
+        var user = "user1";
+
+        await Task.Delay(4_000);
+
+        //Act
+        var httpClient = new HttpClient();
+        httpClient.BaseAddress = new(ClientFactory.HostUrl);
+
+        var getResponse1 = await httpClient.GetAsync($"/actor?user={user}");
+        var contentGetResponse1 = await getResponse1.Content.ReadAsStringAsync();
+        _testOutputHelper.WriteLine($"{nameof(contentGetResponse1)}: {contentGetResponse1}");
+
+        _testOutputHelper.WriteLine("");
+
+        var expectedData = new MyData("property1", "property2");
+        var request = new SetDataRequest(user, expectedData.PropertyA, expectedData.PropertyB);
+        var httpContent = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+        var postResponse = await httpClient.PostAsync("/actor", httpContent);
+        var postResponseContent = await postResponse.Content.ReadAsStringAsync();
+        _testOutputHelper.WriteLine($"postResponseContent: {postResponseContent}");
+
+        _testOutputHelper.WriteLine("");
+
+        var getResponse2 = await httpClient.GetAsync($"/actor?user={user}");
+        var contentGetResponse2 = await getResponse2.Content.ReadAsStringAsync();
+        _testOutputHelper.WriteLine($"{nameof(contentGetResponse2)}: {contentGetResponse2}");
+
+        var loggerHttpClient = new HttpClient();
+        loggerHttpClient.BaseAddress = new($"http://localhost:{Settings.Logger.DaprHttpPort}");
+        loggerHttpClient.DefaultRequestHeaders.Add("dapr-app-id", Settings.Logger.AppId);
+
+        var loggerResponse = await loggerHttpClient.GetAsync("/v1.0/actors/LoggerActor/user1/state/activity");
+        var log = await loggerResponse.Content.ReadAsStringAsync();
+        _testOutputHelper.WriteLine("");
+        _testOutputHelper.WriteLine($"log: {log}");
+
+        //Assert
+        using (new AssertionScope())
         {
-            //Arrange
-            await RedisContainer.StartAsync();
+            getResponse1.Should().HaveStatusCode(HttpStatusCode.NotFound);
+            postResponse.Should().HaveStatusCode(HttpStatusCode.OK);
+            getResponse2.Should().HaveStatusCode(HttpStatusCode.OK);
 
-            await DaprHelper.InitAsync(
-                Settings.Logger.AppId,
-                Settings.Logger.AppPort,
-                Settings.Logger.DaprHttpPort,
-                Settings.Logger.DaprGrpcPort,
-                Settings.Logger.ComponentsPath
-            );
+            var myData = JsonConvert.DeserializeObject<MyData>(contentGetResponse2);
+            myData.Should().BeEquivalentTo(expectedData);
 
-            await DaprHelper.InitAsync(
-                Settings.Service.AppId,
-                Settings.Service.AppPort,
-                Settings.Service.DaprHttpPort,
-                Settings.Service.DaprGrpcPort,
-                Settings.Service.ComponentsPath
-            );
-
-            await DaprHelper.InitAsync(
-                Settings.Client.AppId,
-                Settings.Client.AppPort,
-                Settings.Client.DaprHttpPort,
-                Settings.Client.DaprGrpcPort,
-                Settings.Client.ComponentsPath
-            );
-
-            var clientFactory = new ClientFactory();
-            clientFactory.CreateClient();
-            var clientDaprClient = clientFactory.Services.GetRequiredService<DaprClient>();
-            await clientDaprClient.WaitForSidecarAsync();
-
-            var serviceFactory = new ServiceFactory();
-            serviceFactory.CreateClient();
-            var serviceDaprClient = serviceFactory.Services.GetRequiredService<DaprClient>();
-            await serviceDaprClient.WaitForSidecarAsync();
-
-            var loggerFactory = new LoggerFactory(UtcNow);
-            loggerFactory.CreateClient();
-            var loggerDaprClient = loggerFactory.Services.GetRequiredService<DaprClient>();
-            await loggerDaprClient.WaitForSidecarAsync();
-
-            var user = "user1";
-
-            await Task.Delay(4_000);
-
-            //Act
-            var httpClient = new HttpClient();
-            httpClient.BaseAddress = new(ClientFactory.HostUrl);
-
-            var getResponse1 = await httpClient.GetAsync($"/actor?user={user}");
-            var contentGetResponse1 = await getResponse1.Content.ReadAsStringAsync();
-            _testOutputHelper.WriteLine($"{nameof(contentGetResponse1)}: {contentGetResponse1}");
-
-            _testOutputHelper.WriteLine("");
-
-            var expectedData = new MyData("property1", "property2");
-            var request = new SetDataRequest(user, expectedData.PropertyA, expectedData.PropertyB);
-            var httpContent = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
-            var postResponse = await httpClient.PostAsync("/actor", httpContent);
-            var postResponseContent = await postResponse.Content.ReadAsStringAsync();
-            _testOutputHelper.WriteLine($"postResponseContent: {postResponseContent}");
-
-            _testOutputHelper.WriteLine("");
-
-            var getResponse2 = await httpClient.GetAsync($"/actor?user={user}");
-            var contentGetResponse2 = await getResponse2.Content.ReadAsStringAsync();
-            _testOutputHelper.WriteLine($"{nameof(contentGetResponse2)}: {contentGetResponse2}");
-
-            var loggerHttpClient = new HttpClient();
-            loggerHttpClient.BaseAddress = new($"http://localhost:{Settings.Logger.DaprHttpPort}");
-            loggerHttpClient.DefaultRequestHeaders.Add("dapr-app-id", Settings.Logger.AppId);
-
-            var loggerResponse = await loggerHttpClient.GetAsync("/v1.0/actors/LoggerActor/user1/state/activity");
-            var log = await loggerResponse.Content.ReadAsStringAsync();
-            _testOutputHelper.WriteLine("");
-            _testOutputHelper.WriteLine($"log: {log}");
-
-            //Assert
-            using (new AssertionScope())
-            {
-                getResponse1.Should().HaveStatusCode(HttpStatusCode.NotFound);
-                postResponse.Should().HaveStatusCode(HttpStatusCode.OK);
-                getResponse2.Should().HaveStatusCode(HttpStatusCode.OK);
-
-                var myData = JsonConvert.DeserializeObject<MyData>(contentGetResponse2);
-                myData.Should().BeEquivalentTo(expectedData);
-
-                log.Should().Be($"\"Data updated at {UtcNow}\"");
-            }
-        }
-        finally
-        {
-            //Cleanup
-            await RedisContainer.DisposeAsync();
-
-            await DaprHelper.StopAsync(Settings.Logger.AppId);
-            await DaprHelper.StopAsync(Settings.Service.AppId);
-            await DaprHelper.StopAsync(Settings.Client.AppId);
+            log.Should().Be($"\"Data updated at {IntegrationTestsEnvironment.UtcNow}\"");
         }
     }
 }
